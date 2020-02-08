8dc97b54c7185022c5d39301fe7619152f5c3c44
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-02-08 09:59:29 +0400

    Initial commit

---
+++ src/main/java/lesson02/Main.java
@@ -0,0 +1,15 @@
+package lesson02;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello, "+name+"!");
+        System.out.println("Nice to meet you, "+name+"!");
+        System.out.println("Bye!");
+    }
+}
