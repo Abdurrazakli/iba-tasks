d82ab63cbda6df56f0f6b5e52abce7996bfdedce
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-02-25 10:04:50 +0400

    Lesson 09. warmup t4 implemented

---
+++ src/main/java/lesson09/warmup/t4/Formatter2.java
@@ -0,0 +1,16 @@
+package lesson09.warmup.t4;
+
+
+public class Formatter2{
+    private String message;
+    public Formatter2(String message) {
+        this.message = message;
+    }
+    public Formatter2(Formatter3 fmt3){
+        this.message = fmt3.toString().toUpperCase();
+    }
+    @Override
+    public String toString() {
+        return message.toUpperCase();
+    }
+}
