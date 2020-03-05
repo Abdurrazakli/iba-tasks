b5fef759dc5141589fadb33a328b1c16a3a5b8a0
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-05 08:48:53 +0400

    lesson 13: update: warmup one line solution

--- src/main/java/lesson13/warmup/StringCheckRotated.java
+++ src/main/java/lesson13/warmup/StringCheckRotated.java
@@ -2,7 +2,6 @@ package lesson13.warmup;
 
 public class StringCheckRotated {
   public boolean check(String origin, String rotated) {
-    String originDoubled = origin+origin;
-    return originDoubled.indexOf(rotated) == -1 ? false : true;
+    return new StringBuilder(origin).append(origin).toString().indexOf(rotated) == -1 ? false : true;
   }
 }
