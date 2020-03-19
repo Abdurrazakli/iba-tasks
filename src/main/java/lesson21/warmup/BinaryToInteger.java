c8573bf79c9d79239719527be5eca965a702ddb9
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-19 11:52:31 +0400

    lesson 21 bin to int vice versa

---
+++ src/main/java/lesson21/warmup/BinaryToInteger.java
@@ -0,0 +1,15 @@
+package lesson21.warmup;
+
+public class BinaryToInteger {
+  public static void main(String[] args) {
+    String binary = "00010010"; // 18
+    int result = 0;
+
+    for (int i = 0; i < binary.length() ; i++) {
+      if(binary.charAt(i)=='1')
+      result += 1<<(binary.length() - i - 1);
+    }
+
+    System.out.println(result); // 18
+  }
+}
---
+++ src/main/java/lesson21/warmup/IntegerToBinary.java
@@ -0,0 +1,10 @@
+package lesson21.warmup;
+
+import java.util.Scanner;
+
+public class IntegerToBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+    }
+}
