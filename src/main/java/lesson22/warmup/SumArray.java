a833db7243c3cc553801e3e517667320b1a75f17
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-23 10:09:08 +0400

    lesson 22 recursion

--- src/main/java/lesson22/warmup/SumArray.java
+++ src/main/java/lesson22/warmup/SumArray.java
@@ -7,10 +7,22 @@ public class SumArray {
         int[] arr = {1,2,3,4,6};
         int sum = sumArray(arr);
         System.out.println(sum);
+
+        System.out.println(sumTo(5));
+        System.out.println(factorial(5));
+    }
+
+    private static int factorial(int n) {
+        return n == 0 ? 1 : (n * factorial(n-1));
+    }
+
+    private static int sumTo(int n) {
+        return n==1? 1 : (n + sumTo(n-1));
     }
 
     private static int sumArray(int[] arr) {
         return arr.length == 1 ? arr[0] :
                 arr[arr.length - 1] + sumArray(Arrays.copyOf(arr, arr.length - 1));
     }
+    
 }
